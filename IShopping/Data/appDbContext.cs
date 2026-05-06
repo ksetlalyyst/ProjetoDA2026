74c8e624a14579658cccb1a9125b9aa7e9f292cc
biancam05 <2025165248@my.ipleiria.pt>
2026-05-06 22:52:22 +0100

    Commit codigo
    criação de classes/model/app.config.Falta ligar base de dados.

---
+++ IShopping/Data/appDbContext.cs
@@ -0,0 +1,26 @@
+﻿using iShopping.Models;
+using IShopping.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+
+namespace iShopping.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<ArticleType> ArticleTypes { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Budget> Budgets { get; set; }
+        public DbSet<ShoppingList> ShoppingLists { get; set; }
+        public DbSet<PlannedItem> PlannedItems { get; set; }
+        public DbSet<UnplannedItem> UnplannedItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=localhost;Database=iShoppingDB;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
+    }
+}
